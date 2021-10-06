ec33ddada106591c9c6d23771018b043f93ee006
Yang, Chao-Wei <zeus83157@gmail.com>
2021-10-06 19:55:48 +0800

    加入專案檔案。

---
+++ DI_DecoratorPatternWithGenericInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DI_DecoratorPatternWithGenericInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
