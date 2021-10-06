cbefec7789d740ccdca364b1a5cf399e4c85c2c0
Yang, Chao-Wei <zeus83157@gmail.com>
2021-10-06 20:23:01 +0800

    Add NotifyDecorator.

---
+++ DI_DecoratorPatternWithGenericInterface/Component/NotifyDecorator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Speech.Synthesis;
+using System.Text;
+
+namespace DI_DecoratorPatternWithGenericInterface.Component
+{
+    public class NotifyDecorator<TRequest, TResponse> : IComponent<TRequest, TResponse>
+    {
+        private readonly IComponent<TRequest, TResponse> _component;
+        public NotifyDecorator(IComponent<TRequest, TResponse> component)
+        {
+            _component = component;
+        }
+        public TResponse Execute(TRequest request)
+        {
+            var synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+            synthesizer.Speak("Successfully Send requests");
+            synthesizer.Speak("and receive a response.");
+            return _component.Execute(request);
+        }
+    }
+}
