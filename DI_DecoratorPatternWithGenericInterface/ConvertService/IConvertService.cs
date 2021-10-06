f2041f6e20e900ea59a8d2125d6f2adacadbfa90
Yang, Chao-Wei <zeus83157@gmail.com>
2021-10-06 20:03:40 +0800

    Add ConvertService.

---
+++ DI_DecoratorPatternWithGenericInterface/ConvertService/IConvertService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI_DecoratorPatternWithGenericInterface.ConvertService
+{
+    public interface IConvertService
+    {
+        string SerializeObject(object value);
+        T DeserializeObject<T>(string value);
+    }
+}
---
+++ DI_DecoratorPatternWithGenericInterface/ConvertService/JsonConvertService.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI_DecoratorPatternWithGenericInterface.ConvertService
+{
+    public class JsonConvertService : IConvertService
+    {
+        public T DeserializeObject<T>(string value)
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+
+        public string SerializeObject(object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+}
