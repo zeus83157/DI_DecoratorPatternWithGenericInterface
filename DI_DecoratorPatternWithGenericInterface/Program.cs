f02f6f6085d096a1561889833e8ca58dd343e33c
Yang, Chao-Wei <zeus83157@gmail.com>
2021-10-06 20:24:15 +0800

    Add an example of manually inject instances

--- DI_DecoratorPatternWithGenericInterface/Program.cs
+++ DI_DecoratorPatternWithGenericInterface/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using DI_DecoratorPatternWithGenericInterface.Component;
+using DI_DecoratorPatternWithGenericInterface.ConvertService;
+using DI_DecoratorPatternWithGenericInterface.FakeData;
+using DI_DecoratorPatternWithGenericInterface.Logger;
+using DI_DecoratorPatternWithGenericInterface.Request;
+using DI_DecoratorPatternWithGenericInterface.Response;
+using System;
 
 namespace DI_DecoratorPatternWithGenericInterface
 {
@@ -6,7 +12,27 @@ namespace DI_DecoratorPatternWithGenericInterface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ManualInjectInstancesDemo();
+        }
+
+        static private void ManualInjectInstancesDemo()
+        {
+            // Request001/Response001 or Request002/Response002
+            // Manually inject instances
+            IComponent<Request001, Response001> component =
+                new NotifyDecorator<Request001, Response001>(
+                    new LogDecorator<Request001, Response001>(
+                        new FileLogger(), new JsonConvertService(),
+                        new Component<Request001, Response001>(new JsonConvertService(), new FakeDataService())));
+
+            var data = new Request001
+            {
+                APICode = "001",
+                DateTime = DateTime.Now,
+                Guid = Guid.NewGuid()
+            };
+
+            component.Execute(data);
         }
     }
 }
