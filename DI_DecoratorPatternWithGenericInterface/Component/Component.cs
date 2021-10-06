1a02207d8f5d2a68a876e9320cf72644e1264bfa
Yang, Chao-Wei <zeus83157@gmail.com>
2021-10-06 20:07:08 +0800

    Add Component.

---
+++ DI_DecoratorPatternWithGenericInterface/Component/Component.cs
@@ -0,0 +1,25 @@
+﻿using DI_DecoratorPatternWithGenericInterface.ConvertService;
+using DI_DecoratorPatternWithGenericInterface.FakeData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI_DecoratorPatternWithGenericInterface.Component
+{
+    public class Component<TRequest, TResponse> : IComponent<TRequest, TResponse>
+    {
+        readonly IConvertService _convertService;
+        readonly IFakeDataService _fakeDataService;
+        public Component(IConvertService convertService, IFakeDataService fakeDataService)
+        {
+            _convertService = convertService;
+            _fakeDataService = fakeDataService;
+        }
+
+        public TResponse Execute(TRequest request)
+        {
+            var response = _fakeDataService.GetFakeData(_convertService.SerializeObject(request));
+            return _convertService.DeserializeObject<TResponse>(response);
+        }
+    }
+}
---
+++ DI_DecoratorPatternWithGenericInterface/Component/IComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI_DecoratorPatternWithGenericInterface.Component
+{
+    public interface IComponent<TRequest, TResponse>
+    {
+        TResponse Execute(TRequest request);
+    }
+}
